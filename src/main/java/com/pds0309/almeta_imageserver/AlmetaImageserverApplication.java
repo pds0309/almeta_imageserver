dc63c4ebd849016e786edfad66a833e378b76ab2
pds0309 <76927397+pds0309@users.noreply.github.com>
2022-12-02 15:37:02 +0900

    Initial commit

---
+++ src/main/java/com/pds0309/almeta_imageserver/AlmetaImageserverApplication.java
@@ -0,0 +1,13 @@
+package com.pds0309.almeta_imageserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmetaImageserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlmetaImageserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pds0309/almeta_imageserver/AlmetaImageserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pds0309.almeta_imageserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmetaImageserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
